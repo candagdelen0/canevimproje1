9d9b81c4510b67d05bcf9a011e1336e16cacab1e
candagdelen0 <candagdelen0@gmail.com>
2023-01-25 23:05:32 +0300

    kitabevi | sayfalama fonksiyonları

--- fonksiyon.php
+++ fonksiyon.php
@@ -10,5 +10,13 @@ class sistem {
             return $b = $a->get_result();
         endif;
     }
+    function kategorigetir($vt,$id) {
+        $get="SELECT * FROM kategori WHERE id=$id";
+        return $this->sorgum($vt,$get,1);
+    }
+    function kitapgetir($vt,$katid) {
+        $kiget="SELECT * FROM kategori INNER JOIN kitaplar ON kategori.id = kitaplar.katid WHERE katid";
+        return $this->sorgum($vt,$kiget,1);
+    }
 }
 ?>
